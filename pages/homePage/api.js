7fa96c555b7d2b3bc8713ac0b51588b77af3a64c
Lostleft <1752036320@qq.com>
2019-11-27 15:24:14 +0800

    登录设置

--- pages/homePage/api.js
+++ pages/homePage/api.js
@@ -1,6 +1,9 @@
-import {minRequest} from '/Config/MinRequest.js'
+import {minRequest} from '../../Config/MinRequest.js'
 export class api{
 	static getPic(data){
+	return	minRequest.get('',data)
+	}
+	static getRegion(data){
 	return	minRequest.get('',data)
 	}
 }
