b0583e48ce9647d66e868de7da4c34b51914b05e
yuancaiming <obolozuki@qq.com>
2019-11-28 23:26:30 +0800

    解决问题

--- pages/myOrder/api.js
+++
@@ -1,8 +0,0 @@
-import {minRequest} from '../../Config/MinRequest'
-
-export class api{
-  // 这里统一管理api请求
-  static getData(){
-	  return minRequest.get('/api/WCPCommodityOrder/getAllCommodityOrder')
-  }
-}
