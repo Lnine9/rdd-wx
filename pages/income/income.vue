b1c2f532256bce034580c0c1b0dd2586ae8944ce
yuancaiming <obolozuki@qq.com>
2019-11-28 23:22:20 +0800

    add 部分忘记文件

---
+++ pages/income/income.vue
@@ -0,0 +1,37 @@
+<template>
+	<view id="whole">
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		onLoad(option){
+			let title = '收益';
+			console.log(option.type)
+			if(option.type == 0){
+				title = '返佣收益'
+			}
+			if(option.type == 1){
+				title = '提现记录'
+			}
+			if(option.type == 4){
+				title = '其他收益'
+			}
+			if(option.type == 3){
+				title = '推荐收益'
+			}
+			uni.setNavigationBarTitle({
+				title
+			})
+		}
+	}
+</script>
+
+<style>
+	#whole {
+		width:750rpx;
+		height:1206rpx;
+		background:rgba(248,249,251,1);
+	}
+</style>
