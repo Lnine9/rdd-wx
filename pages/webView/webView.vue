2b667497a9a3aefcac3472d8ca2a964416ae718a
e <785342405@qq.com>
2019-12-01 15:43:37 +0800

    webView

---
+++ pages/webView/webView.vue
@@ -0,0 +1,25 @@
+<template>
+    <view>
+        <web-view :webview-styles="webviewStyles" :src="src"></web-view>
+    </view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				src: 'https://www.baidu.com',
+			}
+		},
+		onLoad(data) {
+			this.src = data.src
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
