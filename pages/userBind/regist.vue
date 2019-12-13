3be6ed305a2437ac0bcb7345bcb43f7bbb694873
majie <1577948619@qq.com>
2019-12-14 00:44:07 +0800

    绑定修改

--- pages/userBind/regist.vue
+++ pages/userBind/regist.vue
@@ -1,6 +1,6 @@
 <template>
 	<view>
-		<web-view v-show="Regist" :webview-styles="webviewStyles" src="http://renduoduo2019.com/register"></web-view>
+		<web-view v-show="Regist" :webview-styles="webviewStyles" src="https://renduoduo2019.com/register"></web-view>
 	</view>
 </template>
 
