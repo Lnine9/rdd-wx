a79d3aec395da6fc4144eeb2f4e8e9ce41f02201
Marswin <mars666.6a@gmail.com>
2020-02-09 21:47:26 +0800

    物流

--- pages/orderDetail/deliver.vue
+++ pages/orderDetail/deliver.vue
@@ -2,163 +2,172 @@
 	<view>
 		<!-- 商品展示 -->
 		<view class="commodity-inner-container">
-			<view class="commodity-img-container">
-				<image :src="imageUrl" mode="" class="commodity-img"></image>
-			</view>
+			<view class="commodity-img-container"><image :src="imageUrl" mode="" class="commodity-img"></image></view>
 			<view class="commodity-content-container">
 				<!-- 商品名 -->
-				<text class="commodity-title">
-					{{order.commodityTitle}}
-				</text>
+				<text class="commodity-title">{{ order.commodityTitle }}</text>
 			</view>
 		</view>
 		<!-- 快递信息 -->
 		<view class="delivery-background">
 			<image class="delivery-image" src="../../static/orderDetail/ic-position.png"></image>
-			<text class="delivery-state">{{requestParams.deliveryCompany}}</text>
-			<text class="delivery-info">快递单号:{{requestParams.deliveryNum}}</text>
+			<text class="delivery-state">{{ requestParams.deliveryCompany }}</text>
+			<text class="delivery-info">快递单号:{{ requestParams.deliveryNum }}</text>
 		</view>
-		<view class="total-wrap" >
+		<view class="total-wrap">
 			<block v-for="(item, index) in tracesData" :key="index">
-				<trackNode :is-first="index===tracesData.length-1" :is-newest="index===0" :is-main-node="item.isMainNode" :node-data="item"></trackNode>
+				<trackNode :is-first="index === tracesData.length - 1" :is-newest="index === 0" :is-main-node="item.isMainNode" :node-data="item"></trackNode>
 			</block>
 		</view>
-		<view class="no-delivery" :style="{display: hasTraces == true ? 'none' : 'block' }">
+		<view class="no-delivery" :style="{ display: hasTraces == true ? 'none' : 'block' }">
 			<image class="no-delivery-image" src="../../static/orderDetail/ic-no-logistics.png"></image>
 			<text class="no-delivery-text">抱歉，暂无此单号轨迹，请稍后再试！</text>
 		</view>
 	</view>
 </template>
 <script>
-	import {DeliverAPI} from './deliverAPI.js'
-	import {OrderDetailAPI} from './api.js'
-    import trackNode from '../components/logisticsTrack/deliver.vue'
-	
-	export default {
-		data() {
-			return {
-				hasTraces: true, // 是否有物流轨迹
-				requestParams: { // 请求物流信息的参数
-					deliveryNum: '',
-					deliveryCompany: ''
-				},
-				orderId:'',
-				order:[],
-				imageUrl:'',
-				tracesData: []
-			}
-		},
-		components: {
-		  trackNode
-		},
-		onLoad: function(params) {
-			if (params) {
-				this.requestParams.deliveryNum = params.deliveryNum;
-				this.requestParams.deliveryCompany = params.deliveryCompany;
-				// this.requestParams.deliveryNum = '75313967739006';
-				// this.requestParams.deliveryCompany = '中通快递';
-				this.orderId = params.orderId;
-				this.judgeTraces(this.requestParams);
-			} else {
-				this.hasTraces = false;
-			}
-			this.getOrderDetail();
-			// 测试
-			// this.requestParams.deliveryNum = '75313967739006';
-			// this.requestParams.deliveryCompany = '中通快递';
-			// this.getTraces();
-			// 
-		},
-		methods: {
-			//判断参数
-			judgeTraces(data){	
-				if(data.deliveryNum !=null && data.deliveryCompany !=null ){
-					this.getTraces();
-				}else{
-					this.requestParams.deliveryNum = '暂无'
-					this.requestParams.deliveryCompany = '暂无'
-					this.hasTraces = false;
-				}
-				console.log(this.requestParams)				
+import { DeliverAPI } from './deliverAPI.js';
+import { OrderDetailAPI } from './api.js';
+import trackNode from '../components/logisticsTrack/deliver.vue';
+
+export default {
+	data() {
+		return {
+			hasTraces: true, // 是否有物流轨迹
+			requestParams: {
+				// 请求物流信息的参数
+				deliveryNum: '',
+				deliveryCompany: ''
 			},
-			// 请求物流轨迹
-			getTraces: function() {
-				let _this = this;
-				DeliverAPI.getDeliverInfo(this.requestParams).then(res => {
+			orderId: '',
+			order: [],
+			imageUrl: '',
+			tracesData: []
+		};
+	},
+	components: {
+		trackNode
+	},
+	onLoad: function(params) {
+		if (params) {
+			this.requestParams.deliveryNum = params.deliveryNum;
+			this.requestParams.deliveryCompany = params.deliveryCompany;
+			// this.requestParams.deliveryNum = '';
+			// this.requestParams.deliveryCompany = '';
+			this.orderId = params.orderId;
+			// this.judgeTraces();
+			this.judgeTraces(this.requestParams);
+		
+		} else {
+			this.hasTraces = false;
+		}
+		this.getOrderDetail();
+		// 测试
+		// this.requestParams.deliveryNum = '75313967739006';
+		// this.requestParams.deliveryCompany = '中通快递';
+		// this.getTraces();
+		//
+	},
+	methods: {
+		//判断参数
+		judgeTraces(data) {
+			console.log(data.deliveryNum == null && data.deliveryCompany == null);
+			console.log(data.deliveryNum != null && data.deliveryCompany != null && data.deliveryNum != '' && data.deliveryCompany != '');
+			if ((data.deliveryNum == null && data.deliveryCompany == null) || (data.deliveryNum == '' && data.deliveryCompany == '')) {
+				// this.getTraces();
+				this.requestParams.deliveryNum = '暂无';
+				this.requestParams.deliveryCompany = '暂无';
+				this.hasTraces = false;
+			} else {
+				this.getTraces();
+				// this.requestParams.deliveryNum = '暂无';
+				// this.requestParams.deliveryCompany = '暂无';
+				// this.hasTraces = false;
+			}
+			console.log(this.requestParams);
+		},
+		// 请求物流轨迹
+		getTraces: function() {
+			let _this = this;
+			DeliverAPI.getDeliverInfo(this.requestParams)
+				.then(res => {
 					if (res.data.data) {
 						console.log(res.data.data.Traces);
-						if(res.data.data.Traces!= null){
-							 for(let i=res.data.data.Traces.length-1; i>=0; i--){
-								 this.tracesData.push(res.data.data.Traces[i]) ;
-							 }
-							 console.log(this.tracesData);
-						}else{
+						if (res.data.data.Traces != null) {
+							for (let i = res.data.data.Traces.length - 1; i >= 0; i--) {
+								this.tracesData.push(res.data.data.Traces[i]);
+							}
+							console.log(this.tracesData);
+						} else {
 							this.tracesData = res.data.data.Traces;
 							this.hasTraces = false;
 						}
 					} else {
 						// todo 没有数据的情况
-						console.log("无物流信息")
+						console.log('无物流信息');
 						this.hasTraces = false;
 					}
-				}).catch(err => {
+				})
+				.catch(err => {
 					console.log(err);
 					this.hasTraces = false;
 					// todo 没有数据的情况
 				});
-			},
-			//获取订单信息
-			getOrderDetail(){
-				let _this = this;
-				OrderDetailAPI.getOrderDetail({
-					orderId:_this.orderId
-				}).then(res => {
-					console.log(res.data.data)
-					this.order = res.data.data;		
+		},
+		//获取订单信息
+		getOrderDetail() {
+			let _this = this;
+			OrderDetailAPI.getOrderDetail({
+				orderId: _this.orderId
+			})
+				.then(res => {
+					console.log(res.data.data);
+					this.order = res.data.data;
 					// 图片地址
-					if(this.order !=null){
+					if (this.order != null) {
 						if (this.order.commodityImgList.length > 0) {
 							this.imageUrl = this.order.commodityImgList[0];
 						}
-					}		
-				}).catch(err => {
+					}
+				})
+				.catch(err => {
 					uni.showToast({
 						title: '获取订单信息失败!',
 						icon: 'none'
 					});
 				});
-			},
-		},
+		}
 	}
+};
 </script>
 
 <style lang="scss" scoped>
 .total-wrap {
-  width: 100vw;
-  height: auto;
-  box-sizing: border-box;
-  padding: 20rpx 120rpx;
-  .logistics-title {
-    height: 72rpx;
-    box-sizing: border-box;
-    padding: 36rpx 0 8rpx;
-    line-height: 28rpx;
-    color: #4B4B4B;
-    font-size: 26rpx;
-    font-family: 'PingFangSC-Medium';
-    text-align: left;
-  }
+	width: 100vw;
+	height: auto;
+	box-sizing: border-box;
+	padding: 20rpx 120rpx;
+	.logistics-title {
+		height: 72rpx;
+		box-sizing: border-box;
+		padding: 36rpx 0 8rpx;
+		line-height: 28rpx;
+		color: #4b4b4b;
+		font-size: 26rpx;
+		font-family: 'PingFangSC-Medium';
+		text-align: left;
+	}
 }
 .commodity-inner-container {
-		display: flex;
-		flex-direction: row;
-		width: 100%;
-		height: 220rpx;
-		background: #FFFFFF;
-	}
+	display: flex;
+	flex-direction: row;
+	width: 100%;
+	height: 220rpx;
+	background: #ffffff;
+}
 .commodity-img-container {
 	margin: auto 30rpx auto 30rpx;
-	border: #E3E3E3 1px solid;
+	border: #e3e3e3 1px solid;
 	border-radius: 10rpx;
 	width: 170rpx;
 	height: 170rpx;
@@ -187,51 +196,51 @@
 	-webkit-box-orient: vertical;
 	-webkit-line-clamp: 2;
 }
-.delivery-background{
+.delivery-background {
 	position: relative;
 	width: 750rpx;
 	height: 150rpx;
 	background-image: url(../../static/orderDetail/ic-background.png);
-	background-position: 50%  0;
+	background-position: 50% 0;
 }
-.delivery-image{
-	width:50rpx;
-	height:73rpx;
+.delivery-image {
+	width: 50rpx;
+	height: 73rpx;
 	margin: 36rpx 36rpx 41rpx 31rpx;
 }
-.delivery-state{
+.delivery-state {
 	position: absolute;
 	top: 38rpx;
-	font-size:32rpx;
-	font-family:SimHei;
-	font-weight:400;
-	color:rgba(255,255,255,1);
+	font-size: 32rpx;
+	font-family: SimHei;
+	font-weight: 400;
+	color: rgba(255, 255, 255, 1);
 }
-.delivery-info{
+.delivery-info {
 	position: absolute;
 	top: 84rpx;
-	font-size:28rpx;
-	font-family:SimHei;
-	font-weight:400;
-	color:rgba(255,255,255,1);
+	font-size: 28rpx;
+	font-family: SimHei;
+	font-weight: 400;
+	color: rgba(255, 255, 255, 1);
 }
-.no-delivery{
+.no-delivery {
 	width: 750rpx;
 	height: 500rpx;
 }
-.no-delivery-image{
+.no-delivery-image {
 	display: inline-block;
 	width: 220rpx;
 	height: 220rpx;
 	margin: 115rpx 265rpx 38rpx 265rpx;
 }
-.no-delivery-text{
+.no-delivery-text {
 	display: inline-block;
 	width: 750rpx;
-	font-size:28rpx;
-	font-family:PingFang;
-	font-weight:500;
+	font-size: 28rpx;
+	font-family: PingFang;
+	font-weight: 500;
 	text-align: center;
-	color:rgba(98,98,98,1);
+	color: rgba(98, 98, 98, 1);
 }
 </style>
