62c10532a59c5c983c163068c3820bf77db93810
lhn <1498613780@qq.com>
2022-11-27 11:25:57 +0800

    1

---
+++ Config/YQRequest.js
@@ -0,0 +1,94 @@
+const config = Symbol('config')
+const isCompleteURL = Symbol('isCompleteURL')
+const requestBefore = Symbol('requestBefore')
+const requestAfter = Symbol('requestAfter')
+
+class YQRequest {
+
+	[config] = {
+		// baseURL: 'http://127.0.0.1:8899',
+		baseURL: 'https://121.37.98.238:8099',
+		dataType: 'json',
+	}
+
+	interceptors = {
+		request: (func) => {
+			if (func) {
+				YQRequest[requestBefore] = func
+			} else {
+				YQRequest[requestBefore] = (request) => request
+			}
+		},
+		response: (func) => {
+			if (func) {
+				YQRequest[requestAfter] = func
+			} else {
+				YQRequest[requestAfter] = (response) => response
+			}
+		}
+	}
+
+	static[requestBefore](config) {
+		return config
+	}
+
+	static[requestAfter](response) {
+		return response
+	}
+
+	static[isCompleteURL](url) {
+		return /(http|https):\/\/([\w.]+\/?)\S*/.test(url)
+	}
+
+	setConfig(func) {
+		this[config] = func(this[config])
+	}
+
+	request(options = {}) {
+		options.baseURL = options.baseURL || this[config].baseURL
+		options.dataType = options.dataType || this[config].dataType
+		options.url = YQRequest[isCompleteURL](options.url) ? options.url : (options.baseURL + options.url)
+		options.data = options.data
+		options.header = { ...options.header
+		}
+		options.method = options.method || this[config].method
+
+		options = { ...options,
+			...YQRequest[requestBefore](options)
+		}
+		return new Promise((resolve, reject) => {
+			options.success = function(res) {
+				resolve(YQRequest[requestAfter](res))
+			}
+			options.fail = function(err) {
+				reject(YQRequest[requestAfter](err))
+			}
+			uni.request(options)
+		})
+	}
+
+	get(url, data, options = {}) {
+		// let token = uni.getStorageSync('token')
+		options.url = url
+		options.data = data
+		options.method = 'GET'
+		options.header = {
+			'content-type': 'application/json;charset=UTF-8',
+			// 'Authorization': token || ''
+		}
+		return this.request(options)
+	}
+	post(url, data, options = {}) {
+		// let token = uni.getStorageSync('token')
+		options.url = url
+		options.data = data
+		options.method = 'POST'
+		options.header = {
+			'content-type': 'application/json;charset=UTF-8',
+			// 'Authorization': token || ''
+		}
+		return this.request(options)
+	}
+}
+
+export const minRequest = new YQRequest()
